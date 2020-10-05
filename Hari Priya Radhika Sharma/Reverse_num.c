cc1686566c0551aea2fff3c25b14488dd69fac52
Hari Priya Radhika Sharma <haripriyaradhika28@gmail.com>
2020-10-05 12:06:00 +0530

    Added folder

---
+++ Hari Priya Radhika Sharma/Reverse_num.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n, dig, rev= 0, digits = 0, n1;
+    scanf("%d", &n);
+    n1 = n;
+    while (n1 != 0)
+    {
+        digits++;
+        n1 = n1 / 10;
+    }
+    while (n != 0)
+    {
+
+        dig = n % 10;
+        rev = rev + (dig * (pow(10, digits - 1)));
+        n/=10;
+        --digits;
+    }
+    printf("%d", rev);
+    return 0;
+}
