9e572e6c715ac2eac656344b63ccc458207dbd9e
Ishita054 <72296363+Ishita054@users.noreply.github.com>
2020-10-10 22:26:37 +0530

    Create pattern.c

---
+++ IshitaSarin/pattern.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+   int i, j,n=5;
+   
+   for(i=1;i<=5;i++)
+   {
+      for(j=1;j<=5;j++)
+      {
+          
+            
+             printf("%d",n);
+       } 
+      n--;
+      printf("\n");
+   }
+  
+   return 0;
+}
+
